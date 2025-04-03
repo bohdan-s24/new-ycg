98b8467f5f94765f3189caa18b16b3483a8dcefc
bohdan-s24 <bohdan.shulhachyk@gmail.com>
2025-04-03 20:58:23 +0200

    remove max_output_tokens

--- api/index.py
+++ api/index.py
@@ -626,7 +626,6 @@ def generate_chapters_with_openai(system_prompt, video_id, formatted_transcript)
                     {"role": "user", "content": enhanced_user_content}
                 ],
                 temperature=0.9,
-                max_output_tokens=2000
             )
             
             chapters = response.choices[0].message.content.strip()
